0a129a578bb85ec6f171372ced6060ec9c00cb0b
pazbarz <178591704+pazbarz@users.noreply.github.com>
2024-10-17 21:24:32 +0200

    Add randomized RGB to mouseover

--- script.js
+++ script.js
@@ -1,18 +1,31 @@
 const sketchContainer = document.querySelector('#sketch-container');
 
-const colorDefault = () => {
+const setDefaultColor = () => {
     const rows = document.querySelectorAll('.row');
     rows.forEach(function (row) {
     row.addEventListener('mouseover', () => {
-        row.style.backgroundColor = 'rgb(0,0,255)';
-        // row.classList.add("colored");
+        const r = Math.floor(Math.random() * 256);
+        const g = Math.floor(Math.random() * 256);
+        const b = Math.floor(Math.random() * 256);
+        row.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
     });
 })};
 
-const SetRgb = function (r, g, b) {
-    
+const pencilBtn = document.querySelector('#pencil-btn');
 
-}
+const setPencilStyle = () => {
+
+    resetGrid();
+    const rows = document.querySelectorAll('.row');
+    rows.forEach(function (row) {
+    row.addEventListener('mouseover', () => {
+        row.style.backgroundColor = "black";
+        //row.style.opacity
+        
+    });
+})};
+
+pencilBtn.addEventListener("click", setPencilStyle);
 
 const createDefaultGrid = function () {
 
@@ -27,12 +40,12 @@ const createDefaultGrid = function () {
             column.appendChild(row);
         }
     }
-    colorDefault();
+    setDefaultColor();
 }
 createDefaultGrid();
 
 
-const sizeBtn = document.querySelector('#grid-size-btn');
+const sizeBtn = document.querySelector('#grid-size-btn'); //create userGrid
 sizeBtn.addEventListener("click", () => { 
 
     let userCol = +prompt(`How many COLUMNS do you want the grid to have?`, 50);
@@ -60,7 +73,7 @@ sizeBtn.addEventListener("click", () => {
                 column.appendChild(row);
             }
         }
-        colorDefault(); 
+        setDefaultColor(); 
     }
     createUserGrid();
 });
